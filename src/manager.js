e80c44c1e5982b4aeb7b4d6f65c187970acb7698
Gaston Hernandez <hcesargaston@hotmail.com>
2023-12-13 23:03:43 -0300

    first commit

---
+++ src/manager.js
@@ -0,0 +1,7 @@
+import { ProductManager } from "../data/ProductManager";
+import { __dirname } from "./utils.js";
+
+
+console.log("dirname", __dirname);
+let myFirstStore = new ProductManager("/productos.json");
+myFirstStore.getProducts().then((data) => console.log(data));
